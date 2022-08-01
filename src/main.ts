00d7eda2000c4a4af1da2af0c22b48e925e878b4
awataki <awataki1206@gmail.com>
2022-08-02 00:52:47 +0900

    impl camera view

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,13 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const appEl:HTMLDivElement = document.querySelector<HTMLDivElement>('#app')!
+const videoEl:HTMLVideoElement = document.createElement('video')
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+navigator.mediaDevices.getUserMedia({ video: { deviceId: localStorage.getItem('deviceId')??'' }}).then(mediaStream =>{
+  videoEl.srcObject = mediaStream
+  const deviceId:string = mediaStream.getVideoTracks()[0]?.getSettings()?.deviceId ?? ''
+  videoEl.play()
+  localStorage.setItem('deviceId',deviceId)
+})
+
+appEl.appendChild(videoEl)
